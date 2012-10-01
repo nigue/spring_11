9da642da6daa039a486009a617f6c4dfc29a534c
nigue <anibaldavor@gmail.com>
2012-10-01 09:30:48 -0300

    commit loko

--- src/main/java/coreservlets/example/advice/afterreturn/AfterReturningLoggingAdvice.java
+++ src/main/java/coreservlets/example/advice/afterreturn/AfterReturningLoggingAdvice.java
@@ -6,10 +6,18 @@ import org.apache.log4j.Logger;
 import org.springframework.aop.AfterReturningAdvice;
 
 public class AfterReturningLoggingAdvice implements AfterReturningAdvice {
+    
+    private static final Logger LOGGER = Logger.getLogger(AfterReturningLoggingAdvice.class);
 
     @Override
     public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
 
+        LOGGER.debug("\n ---AOP--> " + 
+                target.getClass().getSimpleName()
+                + "#" + method.toGenericString()
+                + ". args=" + Arrays.toString(args)
+                + ". exit=return[" + returnValue + "]");
+        
         Logger.getLogger(target.getClass()).debug(
                 target.getClass().getSimpleName()
                 + "#" + method.toGenericString()
